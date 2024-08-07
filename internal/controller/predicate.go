02d8e6ec01af9df39ed17b5bd4052e72fd739750
Arpan Chatterjee <btwseeu78@gmail.com>
2024-08-08 02:58:12 +0530

    Workload Pause Defination

--- internal/controller/predicate.go
+++ internal/controller/predicate.go
@@ -1,8 +1,6 @@
 package controller
 
 import (
-	greenworkloadv1beta1 "github.com/btwseeu78/workload-sleeper/api/v1beta1"
-	"github.com/google/go-cmp/cmp"
 	"sigs.k8s.io/controller-runtime/pkg/event"
 )
 
@@ -17,17 +15,9 @@ func (p *StatusUpdatePredicate) Delete(e event.DeleteEvent) bool {
 }
 
 func (p *StatusUpdatePredicate) Update(e event.UpdateEvent) bool {
-	oldDeployment, ok1 := e.ObjectOld.(*greenworkloadv1beta1.SleepSchedule)
-	newDeployment, ok2 := e.ObjectNew.(*greenworkloadv1beta1.SleepSchedule)
 
-	if !ok1 || !ok2 {
-		return false
-	}
-	if cmp.Equal(oldDeployment.Status.LastTriggered, newDeployment.Status.LastTriggered) {
-		return true
-	}
-	// Compare only the status fields
-	return false
+	return true
+
 }
 
 func (p *StatusUpdatePredicate) Generic(e event.GenericEvent) bool {
